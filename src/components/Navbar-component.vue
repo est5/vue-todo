3bb137e249c1a04047932ca282a705367439cb1f
est5 <ghostrekon5500@ya.ru>
2022-03-24 20:06:17 +0300

    navbar

---
+++ src/components/Navbar-component.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+<template>
+  <nav class="navbar">
+    <h1 class="text-slate-200 font-semibold text-5xl shadow-sm">TODO!</h1>
+    <h2 class="text-1xl font-mono text-slate-200">
+      <a
+        href="https://github.com/est5/vue-todo"
+        class="hover:text-2xl hover:text-slate-100"
+        >Github</a
+      >
+    </h2>
+  </nav>
+</template>
+
+<style>
+.navbar {
+  @apply flex items-center justify-between flex-wrap bg-red-800 p-6 shadow-sm shadow-rose-500;
+}
+</style>
