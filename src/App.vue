af91354f724df562ed9497951d46ac6902a9ca01
est5 <ghostrekon5500@ya.ru>
2022-03-24 19:36:39 +0300

    superultragiga mvp

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,115 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { computed, reactive, ref } from 'vue';
+import type { Todo } from './models/todo';
+let id = 0;
+
+const todos: Todo[] = [
+  {
+    id: id++,
+    text: 'Fit',
+    done: false,
+  },
+  {
+    id: id++,
+    text: 'Get JOB',
+    done: false,
+  },
+  {
+    id: id++,
+    text: 'DE HASLA',
+    done: false,
+  },
+  {
+    id: id++,
+    text: 'Watch F1',
+    done: false,
+  },
+  {
+    id: id++,
+    text: 'Make 6 cool (or not) projects',
+    done: false,
+  },
+];
+const displayTodos = reactive(todos);
+
+const hidden = ref(false);
+
+const toglle = () => {
+  hidden.value = !hidden.value;
+};
+
+const removeTodo = (t: Todo) => {
+  t.done = !t.done;
+};
+
+const createTodo = ref('');
+
+const filteredTodos = computed(() => {
+  if (hidden.value) {
+    return displayTodos.filter((t) => t.done);
+  }
+  return displayTodos.filter((t) => !t.done);
+});
+
+const addTodo = () => {
+  const todo = {
+    id: id++,
+    text: createTodo.value,
+    done: false,
+  };
+  createTodo.value = '';
+  return displayTodos.push(todo);
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="root">
+    <h1>Add to do item</h1>
+    <span class="input-span">
+      <label for="todo-input">Create new todo: </label>
+      <input
+        v-model="createTodo"
+        id="todo-input"
+        type="text"
+        placeholder="type new task"
+      />
+      <button @click="addTodo">ok</button>
+    </span>
+    <ul>
+      <li v-for="t in filteredTodos" :key="t.id">
+        <span class="todo">
+          <button @click="() => removeTodo(t)">X</button> {{ t.text }}
+        </span>
+      </li>
+    </ul>
+    <button @click="toglle">
+      {{ hidden ? 'Show not completed ' : 'Show done todos' }}
+    </button>
+  </div>
 </template>
 
 <style>
-@import './assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+.root {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+h1 {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  color: #1f1f1f;
+}
+.todo {
+  display: flex;
+  gap: 15px;
+  margin-bottom: 3px;
 }
 
-header {
-  line-height: 1.5;
+.input-span {
+  display: flex;
+  gap: 5px;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+ul {
+  list-style: none;
 }
 </style>
